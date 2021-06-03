87dffbc270daacfb5261adfd45e1fa6505f54199
Michael Ruppe <73618042+CoreProduction@users.noreply.github.com>
2021-06-04 08:33:54 +1000

    Update main.py

--- main.py
+++ main.py
@@ -12,26 +12,32 @@ sleep(1)
 
 while True:
     lightVal = colourSensor.read()
-    print(str(lightVal[0]) + " Red  " + str(lightVal[1]) + " Green  " + str(lightVal[2]) + " Blue")
+    red = lightVal[0]
+    grn = lightVal[1]
+    blu = lightVal[2]
+#     print(str(blu) + " Blue  " + str(grn) + " Green  " + str(red) + " Red")
 
 # Uncomment the code below to expose additional functionality
-#     total_colours = lightVal[0] + lightVal[1] +  lightVal[2]
+    total_colours = red + grn +  blu
           
 #     # Extract each colour's contribution
-#     red_c = lightVal[0]/total_colours
-#     green_c = lightVal[1]/total_colours
-#     blue_c = lightVal[2]/total_colours
+    red_c = red/total_colours
+    green_c = grn/total_colours
+    blue_c = blu/total_colours
   
 #     # Categorise the colour being read with a description.  This is a simple demo that has not 
-#     colour_description = "uncategorised"
-#     if ((red_c > 0.4) and (green_c <= 0.4) and (blue_c <= 0.2)): 
-#         colour_description = "red"
-#     if ((red_c <= 0.4) and (green_c > 0.43) and (blue_c <= 0.2)):
-#         colour_description = "green"
-#     if ((red_c <= 0.35) and (green_c <= 0.45) and (blue_c > 0.25)):
-#         colour_description = "blue"
-#     if ((red_c > 0.35) and (red_c <= 0.42) and (green_c > 0.40) and (green_c <= 0.45) and (blue_c < 0.2)):
-#         colour_description = "yellow"
-#     print("{} R  {:5.0f} G  {:5.0f} B  {:5.0f} W  {:5.3f} Lux  {:d} K {} ".format(lightVal[0], lightVal[1], lightVal[2], lightVal[3], lightVal[4], lightVal[5], colour_description))
+    colour_description = "uncategorised"
+    if ((red_c > 0.4) and (green_c <= 0.4) and (blue_c <= 0.3)): 
+        colour_description = "red"
+    if ((red_c <= 0.4) and (green_c > 0.4) and (blue_c <= 0.2)):
+        colour_description = "green"
+    if ((red_c <= 0.35) and (green_c <= 0.45) and (blue_c > 0.25)):
+        colour_description = "blue"
+    if ((red_c > 0.35) and (red_c <= 0.42) and (green_c > 0.40) and (green_c <= 0.45) and (blue_c < 0.2)):
+        colour_description = "yellow"
+    print("{} B  {:5.0f} G  {:5.0f} R makes {}".format(blu, grn, red, colour_description))
     
-    sleep(1)
+    sleep(0.1)
+    
+    
+#     print("{} R  {:5.0f} G  {:5.0f} B  {:5.0f} W  {:5.3f} Lux  {:5.0f} K {} ".format(red, grn, blu, lightVal[3], lightVal[4], lightVal[5], colour_description))
