00576701e6b7f9b3c26f9e828be662ebddc1b50a
Michael Ruppe <73618042+CoreProduction@users.noreply.github.com>
2021-09-17 09:15:30 +1000

    Update to most recent Unified

--- PiicoDev_VEML6040.py
+++ PiicoDev_VEML6040.py
@@ -13,6 +13,7 @@
 
 from PiicoDev_Unified import *
 from utime import sleep_ms
+from math import sqrt
 
 # Registers
 _veml6040Address = 0x10
@@ -27,6 +28,28 @@ _DEFAULT_SETTINGS = b'\x00' # initialise gain:1x, integration 40ms, Green Sensit
 _SHUTDOWN = b'\x01'         # Disable colour sensor
 _INTEGRATION_TIME = 40      # ms
 _G_SENSITIVITY = 0.25168    # lux/step
+
+_NaN=float('NaN')
+
+def rgb2hsv(r, g, b):
+    r = float(r/65535)
+    g = float(g/65535)
+    b = float(b/65535)
+    high = max(r, g, b)
+    low = min(r, g, b)
+    h,s,v = high,high,high
+    d = high - low
+    s = 0 if high == 0 else d/high
+    if high == low:
+        h = 0.0
+    else:
+        h = {
+            r: (g - b) / d+(6 if g < b else 0),
+            g: (b - r) / d+2,
+            b: (r - g) / d+4,
+        }[high]
+        h /= 6
+    return {'hue':h*360,'sat':s, 'val':v}
     
 class PiicoDev_VEML6040(object):
     def __init__(self, bus=None, freq=None, sda=None, scl=None, addr = _veml6040Address):
@@ -45,42 +68,52 @@ class PiicoDev_VEML6040(object):
         except Exception:
             print('Device 0x{:02X} not found'.format(self.addr))
             
+    def classifyHue(self, hues={"red":0,"yellow":60,"green":120,"cyan":180,"blue":240,"magenta":300}, min_brightness=0):
+        d=self.readHSV()
+        if d['val'] > min_brightness:
+            key, val = min(hues.items(), key=lambda x: abs(d['hue'] - x[1]))
+            return key
+        else:
+            return 'None'
+    
     # Read colours from VEML6040
     # Returns raw red, green and blue readings, ambient light [Lux] and colour temperature [K]
-    def read(self):
+    def readRGB(self):
         try:
             raw_data = self.i2c.read16(self.addr, _REG_RED)        # returns a bytes object   
-            data_red_int = int.from_bytes(raw_data, 'little')
+            u16red = int.from_bytes(raw_data, 'little')
             
             raw_data = (self.i2c.read16(self.addr, _REG_GREEN))    # returns a bytes object
-            data_green_int = int.from_bytes(raw_data, 'little')
+            u16grn = int.from_bytes(raw_data, 'little')
             
             raw_data = (self.i2c.read16(self.addr, _REG_BLUE))     # returns a bytes object
-            data_blue_int = int.from_bytes(raw_data, 'little')
+            u16blu = int.from_bytes(raw_data, 'little')
             
             raw_data = (self.i2c.read16(self.addr, _REG_WHITE))    # returns a bytes object
             data_white_int = int.from_bytes(raw_data, 'little')
         except:
             print(i2c_err_str.format(self.addr))
-            return float('NaN'), float('NaN'), float('NaN'), float('NaN'), float('NaN'), float('NaN')
+            return {"red":_NaN,"green":_NaN,"blue":_NaN,"white":_NaN,"als":_NaN,"cct":_NaN}
         
         # Generate the XYZ matrix based on https://www.vishay.com/docs/84331/designingveml6040.pdf
-        colour_X = (-0.023249 * data_red_int) + (0.291014 * data_green_int) + (-0.364880 * data_blue_int)
-        colour_Y = (-0.042799 * data_red_int) + (0.272148 * data_green_int) + (-0.279591 * data_blue_int)
-        colour_Z = (-0.155901 * data_red_int) + (0.251534 * data_green_int) + (-0.076240 * data_blue_int)
-        colour_total = colour_X + colour_Y + colour_Z
+        colour_X = (-0.023249*u16red)+(0.291014*u16grn)+(-0.364880*u16blu)
+        colour_Y = (-0.042799*u16red)+(0.272148*u16grn)+(-0.279591*u16blu)
+        colour_Z = (-0.155901*u16red)+(0.251534*u16grn)+(-0.076240*u16blu)
+        colour_total = colour_X+colour_Y+colour_Z
         if colour_total == 0:
-            return float('NaN'), float('NaN'), float('NaN'), float('NaN'), float('NaN'), float('NaN')
+            return {"red":_NaN,"green":_NaN,"blue":_NaN,"white":_NaN,"als":_NaN,"cct":_NaN}
         else:
             colour_x = colour_X / colour_total
             colour_y = colour_Y / colour_total
         
         # Use McCamy formula
         colour_n = (colour_x - 0.3320)/(0.1858 - colour_y)
-        colour_CCT = 449.0 * colour_n ** 3 + 3525.0 * colour_n ** 2 + 6823.3 * colour_n + 5520.33
+        colour_CCT = 449.0*colour_n ** 3+3525.0*colour_n ** 2+6823.3*colour_n+5520.33
         
         # Calculate ambient light in Lux
-        colour_ALS = data_green_int * _G_SENSITIVITY
-        
-        return data_red_int, data_green_int, data_blue_int, data_white_int, colour_ALS, colour_CCT
-
+        colour_ALS = u16grn*_G_SENSITIVITY
+        return {"red":u16red,"green":u16grn,"blue":u16blu,"white":data_white_int,"als":colour_ALS,"cct":colour_CCT}
+    
+    def readHSV(self):
+        d = self.readRGB()
+        return rgb2hsv(d['red'],d['green'],d['blue'])
--- main.py
+++ main.py
@@ -1,43 +1,32 @@
-# PiicoDev VEML6030 minimal example code
-# This program reads light data from the PiicoDev VEML6040 ambient light sensor
-# and displays the result
+# PiicoDev VEML6040 minimal example code
+# This program reads light data from the PiicoDev VEML6040 Colour Sensor
+# Displays Raw Data, and classifies colours as fruits
 
-import sys
 from PiicoDev_VEML6040 import PiicoDev_VEML6040
-from time import sleep
+from PiicoDev_Unified import sleep_ms
+
+fruitList = {
+    "apple":0,
+    "carrot":60,
+    "lime":120
+    }
 
 colourSensor = PiicoDev_VEML6040()
 
-sleep(1)
-
 while True:
-    lightVal = colourSensor.read()
-    red = lightVal[0]
-    grn = lightVal[1]
-    blu = lightVal[2]
-#     print(str(blu) + " Blue  " + str(grn) + " Green  " + str(red) + " Red")
+    ### Example 1: Print Raw RGB Data
+    data = colourSensor.readRGB() # Read the sensor (Colour space: Red Green Blue)
+    red = data['red'] # extract the RGB information from data
+    grn = data['green']
+    blu = data['blue']
+    
+    print(str(blu) + " Blue  " + str(grn) + " Green  " + str(red) + " Red") # Print the data. Printing as BGR so the Thonny plot-colours match nicely :)
 
-# Uncomment the code below to expose additional functionality
-    total_colours = red + grn +  blu
-          
-#     # Extract each colour's contribution
-    red_c = red/total_colours
-    green_c = grn/total_colours
-    blue_c = blu/total_colours
-  
-#     # Categorise the colour being read with a description.  This is a simple demo that has not 
-    colour_description = "uncategorised"
-    if ((red_c > 0.4) and (green_c <= 0.4) and (blue_c <= 0.3)): 
-        colour_description = "red"
-    if ((red_c <= 0.4) and (green_c > 0.4) and (blue_c <= 0.2)):
-        colour_description = "green"
-    if ((red_c <= 0.35) and (green_c <= 0.45) and (blue_c > 0.25)):
-        colour_description = "blue"
-    if ((red_c > 0.35) and (red_c <= 0.42) and (green_c > 0.40) and (green_c <= 0.45) and (blue_c < 0.2)):
-        colour_description = "yellow"
-    print("{} B  {:5.0f} G  {:5.0f} R makes {}".format(blu, grn, red, colour_description))
+    ### Example 2: Classify the colour being shown - eg. a fruit sorting machine
+    data = colourSensor.readHSV() # Read the sensor (Colour space: Hue Saturation Value)
+    hue = data['hue'] # extract the Hue information from data
     
-    sleep(0.1)
-    
-    
-#     print("{} R  {:5.0f} G  {:5.0f} B  {:5.0f} W  {:5.3f} Lux  {:5.0f} K {} ".format(red, grn, blu, lightVal[3], lightVal[4], lightVal[5], colour_description))
+    label = colourSensor.classifyHue() # Read the sensor again, this time classify the colour
+    print(str(label) + " Hue: " + str(hue)) # Show the label and the corresponding hue
+
+    sleep_ms(1000)
