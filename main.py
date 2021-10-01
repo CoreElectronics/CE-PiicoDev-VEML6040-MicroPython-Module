3f9477f0c6799ffa1f2c8468aa5fc02b8c426b1f
Michael Ruppe <73618042+CoreProduction@users.noreply.github.com>
2021-10-01 14:36:17 +1000

    fix indent, add comments

--- main.py
+++ main.py
@@ -5,13 +5,14 @@
 from PiicoDev_VEML6040 import PiicoDev_VEML6040
 from PiicoDev_Unified import sleep_ms # cross-platform compatible sleep function
 
+# Associate a hue value with a fruit name
 fruitList = {
     "apple":0,
     "carrot":60,
     "lime":120
     }
 
-colourSensor = PiicoDev_VEML6040()
+colourSensor = PiicoDev_VEML6040() # initialise the sensor
 
 while True:
     ### Example 1: Print Raw RGB Data
@@ -23,10 +24,10 @@ while True:
     print(str(blu) + " Blue  " + str(grn) + " Green  " + str(red) + " Red") # Print the data. Printing as BGR so the Thonny plot-colours match nicely :)
 
     ### Example 2: Classify the colour being shown - eg. a fruit sorting machine
-#    data = colourSensor.readHSV() # Read the sensor (Colour space: Hue Saturation Value)
-#    hue = data['hue'] # extract the Hue information from data
-    
-#    label = colourSensor.classifyHue() # Read the sensor again, this time classify the colour
-#    print(str(label) + " Hue: " + str(hue)) # Show the label and the corresponding hue
+#     data = colourSensor.readHSV() # Read the sensor (Colour space: Hue Saturation Value)
+#     hue = data['hue'] # extract the Hue information from data
+# 
+#     label = colourSensor.classifyHue() # Read the sensor again, this time classify the colour
+#     print(str(label) + " Hue: " + str(hue)) # Show the label and the corresponding hue
 
     sleep_ms(1000)
